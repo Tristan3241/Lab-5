70209afd3ca037fe5948484efb5055ea64884926
Tristan Alvey <Tristan.j.alvey-1@ou.edu>
2019-02-14 18:17:08 -0600

    For the last 3 labs/projects I forgot to git init... So I did some googling and realized thats why all my stuff was getting updated to seperate repos so I had more commits but its just now tracking for this repo.

---
+++ Todo.java
@@ -0,0 +1,66 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-02-14
+OU ID_______________: 113290859
+OU EMAIL____________: tristan.j.alvey-1@ou.edu
+
+Tasks:
+1. Implement parseAndExecute
+2. Implement execute
+3. Implement calculateThreeTokens
+4. Implement calculateTwoTokens
+5. Make test case for calculateTwoTokensTestValidHalve()
+6. Make test case for calculateTwoTokensTestValidNegate
+7. Make test case for calculateTwoTokensInvalidNumber
+8. Make test case for calculateTwoTokensTestInvalidCommand()
+9. Make test case for calculateThreeTokensTestValidAdd()
+10. Make test case for calculateThreeTokensTestValidSubtract()
+11. Make test case for calculateThreeTokensTestValidDivide()
+12. Make test case for calculateThreeTokensTestInvalidCommand()
+13. Make test case for executeTestValidQuit()
+14. Make test case for executeTestValidThreeTokens()
+15. Make test case for executeTestInvalidTokenLength()
+16. Make test case for parseAndExecuteTestDivideByZero()
+17. Make test case for parseAndExecuteTestInvalidNumber()
+18. Make test case for parseAndExecuteTestInvalidLength()
+
+Estimated Time (minutes):
+1. 10
+2. 10
+3. 10
+4. 15
+5. 5
+6. 5
+7. 5
+8. 5
+9. 5
+10. 5
+11. 5
+12. 5
+13. 5
+14. 5
+15. 5
+16. 5
+17. 5
+18. 5
+
+Actual Time (minutes):
+1. 8
+2. 15
+3. 8
+4. 20
+5.
+6.
+7.
+8.
+9.
+10.
+11.
+12.
+13.
+14.
+15.
+16.
+17.
+18.
+*/}
